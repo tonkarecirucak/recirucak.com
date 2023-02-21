44116fa1b73868cb160aaf7f81334ac44084b6de
tonkarecirucak <125992893+tonkarecirucak@users.noreply.github.com>
2023-02-21 15:35:14 +0000

    netlify toml bots

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -3,15 +3,17 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import lit from "@astrojs/lit";
-import NetlifyCMS from 'astro-netlify-cms';
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://recirucak.com',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
-  // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit(), 
+  integrations: [
+    sitemap(), 
+    mdx(), 
+    image(), 
+    lit(), 
     NetlifyCMS({
       config: {
         backend: {
@@ -19,12 +21,11 @@ export default defineConfig({
           repo: 'tonkarecirucak/recirucak.com',
           branch: 'main',
         },
-        collections: [
-          // Content collections
-        ],
+        collections: [],
       },
-    })
+    }),
   ], // Add renderers to the config
+
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
