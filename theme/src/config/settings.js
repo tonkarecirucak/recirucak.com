be914444ea2527baabca72ebdfab94ccb8509557
tonkarecirucak <125992893+tonkarecirucak@users.noreply.github.com>
2023-02-21 20:15:45 +0000

    colors

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `Pronađite inspiraciju za pripremu ukusnih jela uz jednostavne recepte, korisne savjete i kreativne ideje za obroke za svaku prigodu. Pridružite se našoj zajednici i otkrijte svijet kulinarskih užitaka s "Tonka, reci ručak"!`,
 	url: `https://recirucak.com`, // No trailing slash!
 	name: `Tonka, reci ručak`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
