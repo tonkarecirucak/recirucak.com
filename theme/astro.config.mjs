bcd563ba94fc37bfb4ab04322ddf16bc92abe9c9
tonkarecirucak <125992893+tonkarecirucak@users.noreply.github.com>
2023-02-21 15:07:03 +0000

    netlify cms

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import lit from "@astrojs/lit";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,23 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
+  integrations: [
+    sitemap(), 
+    mdx(), 
+    image(), 
+    lit(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Content collections
+        ],
+      },
+    }),
+  ], // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
